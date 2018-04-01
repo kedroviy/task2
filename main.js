3b5c8bcc6e9c5a5d8d4bb6d817a5bdb83bcd3f80
Aleksey <kedraja@gmail.com>
2018-04-01 22:50:53 +0300

    get

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ console.log(summ);
 
 // task 1.4
 
-var arr = [0, 0, 0, 1, 3, 2, 5];
+var arr = [2,	1,	1,	2,	2,	2,	0,	0,	2,	0];
 var result = arr.reduce(function(sum, current) {
   return sum + current;
 }, 0);
@@ -47,14 +47,13 @@ console.log(sr);
 
 // task 1.5
 
-var arr = [0, 0, 0, 8, 1, 3, 2, 5, 4]
-
-for (var i = 0; i < arr.length; i++) {
-  if (i % 2 == 0) {
-    console.log ( arr[i] )
+var arr = [0, 0, 0, 8, 12, 3, 2, 5, 4, 22, 44]
+for (var i = 0; i < arr.length; i++){
+  if (arr[i] % 2){
+  	console.log (arr[i])
   }
 }
-  		
+
 
 // task 4
 
