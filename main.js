a8998603e4e2683735198eaeb63592942d1f1af6
Aleksey <36371745+kedroviy@users.noreply.github.com>
2018-11-06 20:38:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,19 @@ for (var i = 0; i < arr.length; i++){
   }
 }
 
+// task 3
+
+var arr = [0, 0, 0, 1, 3, 3, 3, 3, 2, 5];
+var newArr = arr.slice().sort(), most = [undefined, 0], counter = 0;
+
+newArr.reduce(function(old, chr){
+   old == chr ? ++counter > most[1] && (most = [chr, counter]) : (counter = 1)
+   return chr
+});
+
+
+
+console.log( most[0] + " ( "+most[1]+" раза )" );
 
 // task 4
 
